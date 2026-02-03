6fb80fdfb72cde4b0c01c8ddcc84e5296982338c
Imperator Shlepa <serega100480@gmail.com>
2026-02-03 20:28:28 +0100

    На додеп 2 сделана система поджига Потом буду мостить логику удаления топлива из переменной Fuel

--- Content.Shared/_IS/Parilka/ParilkaSystem.cs
+++ Content.Shared/_IS/Parilka/ParilkaSystem.cs
@@ -2,6 +2,9 @@ using Content.Shared.Stacks;
 using Content.Shared.Interaction;
 using Content.Shared.Tag;
 using Robust.Shared.Prototypes;
+using Content.Shared.Item.ItemToggle.Components;
+using Content.Shared.IgnitionSource;
+using Content.Shared.Temperature;
 using System;
 
 
@@ -20,24 +23,39 @@ public sealed class ParilkaSystem : EntitySystem
         SubscribeLocalEvent<ParilkaComponent, InteractUsingEvent>(OnInteractUsing);
     }
 
+    private bool IsHot(EntityUid used)
+    {
+        var ev = new IsHotEvent();
+        RaiseLocalEvent(used, ev);
+        return ev.IsHot;
+    }
 
 
     private void OnInteractUsing(EntityUid uid, ParilkaComponent component, InteractUsingEvent args)
     {
         var used = args.Used;
-        if (!_tag.HasTag(used, "FuelForFire"))
-            return;
-
-
-        if (component.Fuel >= component.MaxFuel)
+        if (_tag.HasTag(used, "FuelForFire"))
+        {
+            if (component.Fuel >= component.MaxFuel)
                 return;
 
-        if (!EntityManager.TryGetComponent<StackComponent>(used, out var stack))
-            return;
-        if (!_stack.Use(used, 1, stack!))
+            if (!EntityManager.TryGetComponent<StackComponent>(used, out var stack))
+                return;
+            if (!_stack.Use(used, 1, stack!))
+                return;
+            component.Fuel = Math.Min(component.Fuel + 1, component.MaxFuel);
+            args.Handled = true;
+        }
+
+
+
+
+        if (!IsHot(used))
             return;
 
-        component.Fuel = Math.Min(component.Fuel + 1, component.MaxFuel);
+        if (component.Fuel <= 0)
+            return;
+        component.Active = true;
         args.Handled = true;
     }
 }
